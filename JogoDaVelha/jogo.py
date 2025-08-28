4aef86a5ed7bcc9965209c035f1224fa305c1e8b
ragielly <ragielly.19@gmail.com>
2025-08-28 18:26:29 -0300

    finalizando exercicio do jogo da velha

--- JogoDaVelha/jogo.py
+++ JogoDaVelha/jogo.py
@@ -26,7 +26,54 @@ def jogada(linha, coluna):
     tabuleiro[linha][coluna] = jogador
     return 'O' if jogador == 'X' else 'X'
 
-    
+def temGanhador():
+
+# """ verifica as linhas """
+    for linha in range(3):
+        if(
+            tabuleiro[linha][0] != ' ' and
+            tabuleiro[linha][0] == tabuleiro[linha][1] and
+            tabuleiro[linha][0] == tabuleiro[linha][2]
+        ):
+            print(f'{tabuleiro[linha][0]} GANHOU!!!')
+            return True
+# """ verifica as coluna """
+    for coluna in range(3):
+        if(
+            tabuleiro[0][coluna] != ' ' and
+            tabuleiro[0][coluna] == tabuleiro[1][coluna] and
+            tabuleiro[0][coluna] == tabuleiro[2][coluna]
+        ):
+            print(f'{tabuleiro[coluna][0]} GANHOU!!')
+            return True
+# verificar diagonal \
+
+    if(
+        tabuleiro[1][1] != ' ' and 
+        tabuleiro[0][0] == tabuleiro[1][1] and
+        tabuleiro[0][0] == tabuleiro[2][2]
+    ):
+        print(f'{tabuleiro[0][0]} GANHOU!!!')
+        return True
+# verificar diagonal /
+
+    if(
+        tabuleiro[1][1] != ' ' and
+        tabuleiro[0][2] == tabuleiro[1][1] and
+        tabuleiro[0][2] == tabuleiro[2][0]
+    ):
+        print(f'{tabuleiro[0][2]} GANHOU!!!')
+        return True
+
+
+def temEmpate():
+    for linhas in range(3):
+        for colunas in range(3):
+            if tabuleiro[linhas][colunas] == ' ':
+               return False
+    print('Acabou em empate!!')
+    return True
+
 while True:
     print(f'Jogador da vez:{jogador}')
     try:
@@ -36,5 +83,7 @@ while True:
     except IndexError:
         print('Digite valores numéricos entre 0 e 2!')
     except ValueError:
-        print('Os valores devem ser números inteiros!')
+        print('Os valores devem ser números inteiros!')        
     exibeTabuleiro()
+    if temGanhador() or temEmpate():
+        break
