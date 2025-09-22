dfbced108910ec76b196649b3c9c6bcf37cf7697
ragielly <ragielly.19@gmail.com>
2025-09-22 18:38:16 -0300

    exemplo salario

---
+++ salario-mensal.py
@@ -0,0 +1,20 @@
+#Variaveis 
+
+#numeros 
+velocidade_internet = 10
+nota_filme=8.5 #floatS
+#valores boolanos
+esta_aberto= True
+#strings
+nome_curso= 'Logica de Programação'
+
+#Escreva um programa que calcule o valor hora de um funcionario
+#tendo base eu salario mensal e carga horária
+
+salario_mensal = int(input('Digite seu salário mensal: '))
+carga_horaria = int(input('Digite sua carga horária:'))
+
+valor_hora = salario_mensal / carga_horaria
+
+print(f'Seu valor hora é :{valor_hora}')
+
